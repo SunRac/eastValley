947316d08db2b861270797c424a04cdd8c9b89d4
SunRac <shjsnr@163.com>
2019-09-28 18:52:25 +0800

    引入springmvc依赖，并启用mvc

---
+++ eastValley/src/main/java/com/eastValley/demo/demo.java
@@ -0,0 +1,14 @@
+package com.eastValley.demo;
+
+/**
+ * @author java_shj
+ * @desc
+ * @createTime 2019/9/27 10:59
+ **/
+public class demo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Math.random());
+        }
+    }
+}
---
+++ eastValley/src/main/java/com/eastValley/javaConfig/EastValleyWebAppInitializer.java
@@ -0,0 +1,31 @@
+package com.eastValley.javaConfig;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author java_shj
+ * @desc    使用java配置类，基于springMVC配置web项目应用上下文
+ * @createTime 2019/9/28 11:06
+ **/
+public class EastValleyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
+
+
+
+
+
